583d882c5cf3e103cb472d4c97daa6afa0fbdf0c
yaxiong sun <114034545+sunyaxiong1996@users.noreply.github.com>
2023-07-24 16:49:08 +0900

    comot

--- for 循环的基础语法.py
+++ for 循环的基础语法.py
@@ -1,14 +1,16 @@
 # for 循环的基础语法
 # 逐个来循环
+# 遍历字符串
+
 name = "sunyaxiong"
 for x in name:
     print(x)
     
+print("---------------------------------------------")
 
-
-
-
-
+yanan = "chianese"
+for y in yanan:
+    print(y)
 
 
 
