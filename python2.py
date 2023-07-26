a640366b061646a090044bfce3c1420689ae9abe
yaxiong sun <114034545+sunyaxiong1996@users.noreply.github.com>
2023-07-26 14:01:28 +0900

    更新 python2.py
    man

--- python2.py
+++ python2.py
@@ -1,5 +1,5 @@
 # 数据容器   数组 包含多个元素的数字综合
-
+#
 
 
 
@@ -12,9 +12,18 @@
 # 案例
 
 name_list = ['itheima','itcast','python']
+print(name_list)
+print(type(name_list))
 
-  
-  
+# 列表的嵌套
+# 案例
+my_list = [[1,2,3],[4,5,6]]
+print(my_list)
+print(type(my_list))
+# 答案
+# [[1,2,3],[4,5,6]]
+# <class 'list'>
+
   
   
   
