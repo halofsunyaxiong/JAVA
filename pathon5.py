8cd346ae69752afa7f2904fde9505800d2c9f429
yaxiong sun <114034545+sunyaxiong1996@users.noreply.github.com>
2023-08-01 22:02:40 +0900

    number

---
+++ pathon5.py
@@ -0,0 +1,35 @@
+# 01两数之和
+number1 =1.5
+number2 =12
+sum = number1 + number2
+print(f'The sum of two numbers are:{number1}+{number2}={sum}')
+
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
