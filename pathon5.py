ae1135df5d6199dd96f5585d5205840ba500073b
yaxiong sun <114034545+sunyaxiong1996@users.noreply.github.com>
2023-08-01 22:24:13 +0900

    attribute

--- pathon5.py
+++ pathon5.py
@@ -4,12 +4,22 @@ number2 =12
 sum = number1 + number2
 print(f'The sum of two numbers are:{number1}+{number2}={sum}')
 
-#
+# 阶乘
+def get_jc(number):
+    result = 1
+    while number > 0:
+        result *= number
+        number -= 1
+    return result
+print('阶乘6的值为:',get_jc(6))
+print('阶乘50的值为:',get_jc(50))
+print('阶乘100的值为:',get_jc(100))
 
-
-
-
-
+'''
+阶乘6的值为: 720
+阶乘50的值为: 30414093201713378043612608166064768844377641568960512000000000000
+阶乘100的值为: 93326215443944152681699238856266700490715968264381621468
+'''
 
 
 
